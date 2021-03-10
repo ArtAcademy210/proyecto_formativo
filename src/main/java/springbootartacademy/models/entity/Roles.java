7cfc5e2a795649849c4aa2a2c3358d4ddf447df2
Osneider <escritura.100.com@gmail.com>
2021-03-10 13:48:52 -0500

    version01

---
+++ src/main/java/springbootartacademy/models/entity/Roles.java
@@ -0,0 +1,35 @@
+package springbootartacademy.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import com.sun.istack.NotNull;
+
+
+
+@Entity
+public class Roles {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@Column(length=45, nullable=false, unique=true)
+	private String nombre;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+
+}
